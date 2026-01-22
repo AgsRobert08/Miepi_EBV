4db21262c4889b09297c9b7b65df76a9c6a91e98
AgsRobert08 <betoazhueyapa@gmail.com>
2026-01-21 23:22:02 -0600

    Automatizar creacion de superusuario

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Intentar crear el superusuario (el "|| true" evita que falle si ya existe)
+python manage.py createsuperuser --no-input || true
